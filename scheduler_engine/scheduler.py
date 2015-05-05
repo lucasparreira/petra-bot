de8a9f25d74d86cff68d805f5c5760e6c636a5d0
lucas <lucas.s.parreira@gmail.com>
2015-05-04 22:02:50 -0300

    Setup.

---
+++ scheduler_engine/scheduler.py
@@ -0,0 +1,19 @@
+import time
+import json
+
+class Scheduler(object):
+
+    def start(self):
+
+        params = self.get_parameters()
+
+        while (True):
+            self.scale_and_run_spiders(params)
+            time.sleep(params['run_every'] * 60)
+
+    def get_parameters(self):
+        with open('config.json', 'r') as f:
+            return json.load(f)
+
+    def scale_and_run_spiders(self, params):
+        print ('scale_and_run_spiders')
---
+++ scheduler_service.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+from scheduler_engine.scheduler import Scheduler
+
+if __name__ == '__main__':
+    Scheduler().start()
